a6643f195c48830798a8196d8bfa544cef63820d
dan-avram-selab <dan.avram@mta.ro>
2020-10-26 09:31:43 +0200

    Implementare clasa Utilizator
    Signed-off-by: dan-avram-selab <dan.avram@mta.ro>

---
+++ src/ro/mta/facc/selab/Utilizator.java
@@ -0,0 +1,12 @@
+package ro.mta.facc.selab;
+
+import java.util.ArrayList;
+
+public class Utilizator {
+    String nume;
+    ArrayList<Carte> cartiImprumutate;
+
+    public Utilizator(String nume) {
+        this.nume = nume;
+    }
+}
